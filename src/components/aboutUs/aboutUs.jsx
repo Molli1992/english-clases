22e3ad04349a807970e378bf44980459b2c3033f
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-06-05 16:55:19 -0300

    sobre mi

--- src/components/aboutUs/aboutUs.jsx
+++ src/components/aboutUs/aboutUs.jsx
@@ -3,6 +3,8 @@ import styles from "./aboutUs.module.css";
 import Title from "@/components/title/title";
 import BlueButton from "@/components/buttons/blueButton";
 import { RouteGoContact } from "@/utils/utils";
+import Image from "next/image";
+import TeacherImg from "@/assets/images/teacher.jpg";
 
 export default function AboutUs() {
   const { goContact } = RouteGoContact();
@@ -10,14 +12,14 @@ export default function AboutUs() {
   return (
     <div className={`${styles.body} padding`}>
       <div className={`${styles.container} width`}>
-        <div className={styles.subContainer}>
-          <div className={styles.img} />
+        <div className={styles.subContainer} style={{ alignItems: "center" }}>
+          <Image src={TeacherImg} alt="Sobre mi" className={styles.img} />
         </div>
 
         <div className={styles.subContainer}>
           <Title value="sobre mi" />
-          <h1 className={styles.title}>Hola, soy tu profesora de inglés</h1>
-          <p className={styles.text}>
+          <h1 className="mainTitle">Hola, soy tu profesora de inglés</h1>
+          <p className="mainText">
             Soy profesora de inglés con más de 10 años de experiencia enseñando
             a personas de todas las edades. Mi misión es ayudarte a alcanzar tus
             metas con clases personalizadas, dinámicas y enfocadas en la
