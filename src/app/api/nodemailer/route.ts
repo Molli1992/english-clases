814d528f6c2c2d63776c69475e45d3e11ec579fb
Felipe Blaksley <felipe.blaksley@hotmail.com>
2025-06-05 12:42:57 -0300

    arreglo API

--- src/app/api/nodemailer/route.ts
+++ src/app/api/nodemailer/route.ts
@@ -1,14 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const sendEmail = async (req: NextRequest, res: NextResponse) => {
+export async function POST(req: NextRequest) {
   try {
     return NextResponse.json({ message: "Email sent" }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { message: error || "Error sending email", error },
+      { message: "Error sending email", error },
       { status: 500 }
     );
   }
-};
-
-export { sendEmail as POST };
+}
